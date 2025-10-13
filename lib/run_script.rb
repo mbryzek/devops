a35057df4f18fe22d9bb37c825a631593fae3dc0
Michael Bryzek <mbryzek@alum.mit.edu>
2025-10-13 19:53:08 -0400

    wip

--- lib/run_script.rb
+++ lib/run_script.rb
@@ -1,22 +1,15 @@
 class RunScript
 
-    def initialize(config, file, vars, index)
+    def initialize(config, file, vars, nodes, index)
         @config = config
         @file = file
-        @vars = RunScript.with_deployment_node_index(vars, index)
+        @vars = vars.with_variable("DEPLOYMENT_NODE_INDEX", index.to_s).withVariable("DEPLOYMENT_NODES", nodes.join(","))
     end
 
-    def RunScript.with_deployment_node_index(vars, index)
-        vars.with_variable("DEPLOYMENT_NODE_INDEX", index.to_s)
-    end
-
-    def to_file(name, index)
-        dir = File.basename(@file).sub(/\.tar\.gz$/, "")
-
-        env = RunScript.with_deployment_node_index(@vars, index)
+    def to_file(name)
         File.open(name, "w") do |out|
           out << "#!/usr/bin/env sh\n\n"
-          out << "#{env.serialize("sh")} bin/#{@config.scala.dist_run_script_name} \"-Dhttp.port=#{@config.port}\"\n"
+          out << "#{@vars.serialize("sh")} bin/#{@config.scala.dist_run_script_name} \"-Dhttp.port=#{@config.port}\"\n"
         end
         Util.run("chmod +x #{name}")
         nil
