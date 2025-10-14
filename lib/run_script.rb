ed10242212ab690ec4bb06016ef5bf9019d468be
Michael Bryzek <mbryzek@alum.mit.edu>
2025-10-13 21:39:40 -0400

    Expose nodes

--- lib/run_script.rb
+++ lib/run_script.rb
@@ -1,24 +1,21 @@
 class RunScript
 
-    def initialize(config, file, vars, index)
-        @config = config
-        @file = file
-        @vars = RunScript.with_deployment_node_index(vars, index)
-    end
+  attr_reader :vars
 
-    def RunScript.with_deployment_node_index(vars, index)
-        vars.with_variable("DEPLOYMENT_NODE_INDEX", index.to_s)
-    end
+  def initialize(config, file, vars, nodes, index)
+    @config = config
+    @file = file
+    port_string = config.port == 80 ? '' : ":#{config.port}"
+    all_nodes = nodes.map { |n| "#{n.uri}#{port_string}" }
+    @vars = vars.with_variable("DEPLOYMENT_NODE_INDEX", index.to_s).with_variable("DEPLOYMENT_NODES", all_nodes.join(","))
+  end
 
-    def to_file(name, index)
-        dir = File.basename(@file).sub(/\.tar\.gz$/, "")
-
-        env = RunScript.with_deployment_node_index(@vars, index)
-        File.open(name, "w") do |out|
-          out << "#!/usr/bin/env sh\n\n"
-          out << "#{env.serialize("sh")} bin/#{@config.scala.dist_run_script_name} \"-Dhttp.port=#{@config.port}\"\n"
-        end
-        Util.run("chmod +x #{name}")
-        nil
+  def to_file(name)
+    File.open(name, "w") do |out|
+      out << "#!/usr/bin/env sh\n\n"
+      out << "#{@vars.serialize("sh")} bin/#{@config.scala.dist_run_script_name} \"-Dhttp.port=#{@config.port}\"\n"
     end
+    Util.run("chmod +x #{name}")
+    nil
+  end
 end
