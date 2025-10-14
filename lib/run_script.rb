b49dd08e43bd1d89b2cc314af726ea3cb12ba33e
Michael Bryzek <mbryzek@alum.mit.edu>
2025-10-13 20:02:38 -0400

    wip

--- lib/run_script.rb
+++ lib/run_script.rb
@@ -2,18 +2,18 @@ class RunScript
 
   attr_reader :vars
 
-    def initialize(config, file, vars, nodes, index)
-        @config = config
-        @file = file
-        @vars = vars.with_variable("DEPLOYMENT_NODE_INDEX", index.to_s).with_variable("DEPLOYMENT_NODES", nodes.join(","))
-    end
+  def initialize(config, file, vars, nodes, index)
+    @config = config
+    @file = file
+    @vars = vars.with_variable("DEPLOYMENT_NODE_INDEX", index.to_s).with_variable("DEPLOYMENT_NODES", nodes.join(","))
+  end
 
-    def to_file(name)
-        File.open(name, "w") do |out|
-          out << "#!/usr/bin/env sh\n\n"
-          out << "#{@vars.serialize("sh")} bin/#{@config.scala.dist_run_script_name} \"-Dhttp.port=#{@config.port}\"\n"
-        end
-        Util.run("chmod +x #{name}")
-        nil
+  def to_file(name)
+    File.open(name, "w") do |out|
+      out << "#!/usr/bin/env sh\n\n"
+      out << "#{@vars.serialize("sh")} bin/#{@config.scala.dist_run_script_name} \"-Dhttp.port=#{@config.port}\"\n"
     end
+    Util.run("chmod +x #{name}")
+    nil
+  end
 end
